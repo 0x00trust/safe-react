f2781c1ac6d50104a83d111f4aa865f4d173d5cb
iamacook <aaron.cook@gnosis.pm>
2021-09-10 11:17:01 +0200

    Adjust legacy route detection + fix tests

--- src/routes/legacy/components/ChainRoute/__tests__/index.test.tsx
+++ src/routes/legacy/components/ChainRoute/__tests__/index.test.tsx
@@ -1,67 +1,45 @@
 import React from 'react'
 import { render, waitFor, screen } from '@testing-library/react'
 import { generatePath, Router } from 'react-router'
-import { getNetworkNameSlug, SAFE_ROUTES } from 'src/routes/routes'
 import ChainRoute from '..'
 import { LEGACY_SAFE_ROUTES } from 'src/routes/legacy/routes'
 import { createMemoryHistory } from 'history'
+import { SAFE_ROUTES, getNetworkNameSlug } from 'src/routes/routes'
 
 describe('ChainRoute', () => {
-  const routes = Object.values(SAFE_ROUTES)
-  const legacyRoutes = Object.values(LEGACY_SAFE_ROUTES)
+  const currentRoute = generatePath(SAFE_ROUTES.ASSETS_BASE_ROUTE, {
+    networkName: getNetworkNameSlug(),
+    safeAddress: 'testSafeAddress',
+  })
+  const legacyRoute = generatePath(LEGACY_SAFE_ROUTES.ASSETS_BASE_ROUTE, {
+    networkName: getNetworkNameSlug(),
+    safeAddress: 'testSafeAddress',
+  })
 
-  const getRandomRoute = (routes: string[]) => {
-    const randomIndex = Math.floor(Math.random() * routes.length)
-    return generatePath(routes[randomIndex], { networkName: getNetworkNameSlug(), safeAddress: 'xyz' })
-  }
-
-  // Must add providers as Router is there with custom history
   const renderInRouter = (path: string | string[]) => {
-    const history = createMemoryHistory()
+    const browserHistory = createMemoryHistory()
+
     render(
-      <Router history={history}>
-        <ChainRoute path={path} render={({ location }) => <>{location.pathname}</>} />
+      <Router history={browserHistory}>
+        <ChainRoute path={path} />
       </Router>,
     )
-    return history
+
+    return browserHistory
   }
   it('should render any route if only one is given', async () => {
     const testRoute = '/test'
 
-    const history = renderInRouter(testRoute)
-    history.push(testRoute)
-
-    await waitFor(() => {
-      // const route = screen.findByText(testRoute)
-      // expect(route).toBeInTheDocument()
-      expect(history.location.pathname).toBe(testRoute)
-    })
+    const { location } = renderInRouter(testRoute)
+    expect(location.pathname).toBe(testRoute)
   })
   it('should render routes containing the /:networkName slug', async () => {
-    const currentRoute = getRandomRoute(routes)
-    const legacyRoute = getRandomRoute(legacyRoutes)
-
-    const history = renderInRouter([currentRoute, legacyRoute])
-    history.push(legacyRoute)
-
-    await waitFor(() => {
-      // const route = screen.findByText(currentRoute)
-      // expect(route).toBeInTheDocument()
-      expect(history.location.pathname).toBe(currentRoute)
-    })
+    const { location } = renderInRouter([currentRoute, legacyRoute])
+    expect(location.pathname).toBe(currentRoute)
   })
 
   it('should render routes containing the current chain name', async () => {
-    const currentRoute = getRandomRoute(routes)
-    const legacyRoute = getRandomRoute(legacyRoutes)
-
-    const history = renderInRouter([currentRoute, legacyRoute])
-    history.push(legacyRoute)
-
-    await waitFor(() => {
-      // const route = screen.findByText(currentRoute)
-      // expect(route).toBeInTheDocument()
-      expect(history.location.pathname).toBe(currentRoute)
-    })
+    const { location } = renderInRouter([currentRoute, legacyRoute])
+    expect(location.pathname).toBe(currentRoute)
   })
 })
